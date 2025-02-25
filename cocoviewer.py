0820daea48a3a9e1a0e12089bc0b4c0dc38bf540
Bingliang Li <49446651+BingliangLi@users.noreply.github.com>
2025-02-25 20:14:16 +0800

    Update cocoviewer.py

--- cocoviewer.py
+++ cocoviewer.py
@@ -180,7 +180,10 @@ def draw_bboxes(draw, objects, labels, obj_categories, ignore, width, label_size
                     # TODO: Implement notification message as popup window
                     font = ImageFont.load_default()
 
-                tw, th = draw.textsize(text, font)
+                # Replace deprecated textsize with textbbox
+                # Get the bounding box of the text
+                left, top, right, bottom = draw.textbbox((0, 0), text, font=font)
+                tw, th = right - left, bottom - top
                 tx0 = b[0]
                 ty0 = b[1] - th
 
